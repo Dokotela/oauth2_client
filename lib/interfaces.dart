bc8350e2e024b2a2fd71d0cf352fee6ab08f3708
Dokotela <grey.faulkenberry@mayjuun.com>
2022-05-09 11:57:28 +0200

    Expose SecureStorage Interface

--- lib/interfaces.dart
+++ lib/interfaces.dart
@@ -1,3 +1,4 @@
 export 'package:oauth2_client/src/base_web_auth.dart';
 export 'package:oauth2_client/src/base_storage.dart';
 export 'package:oauth2_client/src/token_storage.dart';
+export 'package:oauth2_client/src/secure_storage.dart';
